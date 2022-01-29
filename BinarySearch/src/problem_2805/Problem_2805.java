998b6f279a50a8df9e51da43db4e01dddbce3b6b
DaWoon Lee <qkgk61@gmail.com>
2022-01-29 17:58:21 +0900

    solve binary search ; gold 3, 2805

---
+++ BinarySearch/src/problem_2805/Problem_2805.java
@@ -0,0 +1,65 @@
+package problem_2805;
+
+import java.util.Scanner;
+
+public class Problem_2805 {
+    private static int N;
+    private static int M;
+    private static int[] trees;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        M = sc.nextInt();
+
+        trees = new int[N];
+        for (int i = 0; i < N; i++) {
+            trees[i] = sc.nextInt();
+        }
+
+        int maxheight = 0;
+        for (int tree : trees) {
+            if (tree > maxheight) {
+                maxheight = tree;
+            }
+        }
+
+        System.out.println(binarySearch(0, maxheight));
+    }
+
+    private static int binarySearch(int left, int right) {
+        int mid = left + (right - left) / 2;
+
+        // 종료 조건
+        if (right - left <= 1) {
+            if(isHeight(right)) {
+                return right;
+            }
+            return left;
+        }
+
+        if (isHeight(mid)) {
+            return binarySearch(mid, right);
+        } else {
+            return binarySearch(left, mid - 1);
+        }
+    }
+
+    private static boolean isHeight(int height) {
+        long sum = 0;
+
+        for (int i = N - 1; i >= 0; i--) {
+            if (trees[i] <= height) {
+                continue;
+            }
+
+            sum += trees[i] - height;
+        }
+
+        if (sum >= M) {
+            return true;
+        }
+
+        return false;
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # BAEKJOON
+
+## Solving
+### Non
+
+## Solved
+### Basic
+while(Bronze 1) : 1100, https://www.acmicpc.net/problem/1110
+
+### Divide and Conquer
+Silver 3 : 2630, https://www.acmicpc.net/problem/2630
+
+### Binary Search
+#### find target
+Silver 4 : 1920, https://www.acmicpc.net/problem/1920
+
+#### find max or min
+Silver 3 : 2805, https://www.acmicpc.net/problem/2805  
+Gold 5 : 2110, https://www.acmicpc.net/problem/2110  
+Gold 2 : 1300, https://www.acmicpc.net/problem/1300
+
+## Give Up
+### Non
