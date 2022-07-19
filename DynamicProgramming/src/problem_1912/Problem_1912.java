731b31228cca68d7cc6cfc913b0548f4be259a48
이다운 <innocent61@naver.com>
2022-07-19 23:38:12 +0900

    Fix code

--- DynamicProgramming/src/problem_1912/Problem_1912.java
+++ DynamicProgramming/src/problem_1912/Problem_1912.java
@@ -4,34 +4,22 @@ import java.io.*;
 import java.util.*;
 
 public class Problem_1912 {
-    private static int n;
-    private static int[] nums;
-    private static int[] tabulate;
-
     public static void main(String[] args) throws IOException {
         BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
 
-        n = Integer.parseInt(input.readLine());
-        nums = new int[n];
-        tabulate = new int[n];
+        int n = Integer.parseInt(input.readLine());
+        int[] tabulate = new int[n];
 
         StringTokenizer tokenizer = new StringTokenizer(input.readLine());
-        for (int i = 0; i < n; i++) {
-            nums[i] = Integer.parseInt(tokenizer.nextToken());
-        }
-
-        System.out.print(maxContiSum());
-    }
-
-    private static int maxContiSum() {
-        tabulate[0] = nums[0];
+        tabulate[0] = Integer.parseInt(tokenizer.nextToken());
         int max = tabulate[0];
 
         for (int i = 1; i < n; i++) {
-            tabulate[i] = Math.max(tabulate[i - 1] + nums[i], nums[i]);
+            int num = Integer.parseInt(tokenizer.nextToken());
+            tabulate[i] = Math.max(tabulate[i - 1] + num, num);
             max = Math.max(max, tabulate[i]);
         }
 
-        return max;
+        System.out.print(max);
     }
 }
