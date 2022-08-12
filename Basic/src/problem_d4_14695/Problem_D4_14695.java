421f435e119637999d177510bea52c373e816430
DaWoon Lee <qkgk61@gmail.com>
2022-08-13 03:40:17 +0900

    Solve math ; D4, 14695

---
+++ Basic/src/problem_d4_14695/Problem_D4_14695.java
@@ -0,0 +1,113 @@
+package problem_d4_14695;
+
+import java.io.*;
+import java.util.*;
+
+public class Problem_D4_14695 {
+    private static class Vector {
+        private long x;
+        private long y;
+        private long z;
+
+        public Vector(long x, long y, long z) {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        public static Vector outerProduct(Vector v1, Vector v2) {
+            long newX = v1.y * v2.z - v1.z * v2.y;
+            long newY = v1.z * v2.x - v1.x * v2.z;
+            long newZ = v1.x * v2.y - v1.y * v2.x;
+
+            return new Vector(newX, newY, newZ);
+        }
+
+        public static long innerProduct(Vector v1, Vector v2) {
+            return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z;
+        }
+
+        public static boolean isParallel(Vector v1, Vector v2) {
+            Vector outerProduct = outerProduct(v1, v2);
+            return norm(outerProduct) == 0;
+        }
+
+        public static long norm(Vector v) {
+            return Math.abs(v.x) + Math.abs(v.y) + Math.abs(v.z);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder answer = new StringBuilder();
+
+        final int T = Integer.parseInt(input.readLine());
+        loop : for (int tc = 1; tc <= T; tc++) {
+            answer.append("#").append(tc).append(' ');
+
+            final int N = Integer.parseInt(input.readLine());
+
+            if (N <= 3) {
+                for (int i = 0; i < N; i++) {
+                    input.readLine();
+                }
+
+                answer.append("TAK\n");
+                continue;
+            }
+
+            StringTokenizer st = new StringTokenizer(input.readLine());
+            long x = Long.parseLong(st.nextToken());
+            long y = Long.parseLong(st.nextToken());
+            long z = Long.parseLong(st.nextToken());
+            Vector move = new Vector(x, y, z);
+
+            List<Vector> vectors = new ArrayList<>();
+            for (int i = 1; i < N; i++) {
+                st = new StringTokenizer(input.readLine());
+                x = Long.parseLong(st.nextToken());
+                y = Long.parseLong(st.nextToken());
+                z = Long.parseLong(st.nextToken());
+
+                vectors.add(new Vector(x - move.x, y - move.y, z - move.z));
+            }
+
+            Vector v1 = vectors.remove(vectors.size() - 1);
+            deleteParallelVector(vectors, v1);
+
+            if (vectors.isEmpty()) {
+                answer.append("TAK\n");
+                continue;
+            }
+
+            Vector v2 = vectors.remove(vectors.size() - 1);
+            deleteParallelVector(vectors, v2);
+
+            if (vectors.isEmpty()) {
+                answer.append("TAK\n");
+                continue;
+            }
+
+            Vector normal = Vector.outerProduct(v1, v2);
+
+            for (Vector cur : vectors) {
+                if (Vector.innerProduct(normal, cur) != 0) {
+                    answer.append("NIE\n");
+                    continue loop;
+                }
+            }
+
+            answer.append("TAK\n");
+        }
+
+        System.out.println(answer);
+    }
+
+    private static void deleteParallelVector(List<Vector> vectors, Vector v) {
+        for (int i = vectors.size() - 1; i >= 0; i--) {
+            if (Vector.isParallel(v, vectors.get(i))) {
+                vectors.remove(i);
+            }
+        }
+    }
+}
