b9de95df23d6b770b029593d8bdec87e1f2fb5d6
DaWoon Lee <qkgk61@gmail.com>
2022-09-06 01:15:42 +0900

    Solve hash map

---
+++ Basic/src/problem_tree1_9/Problem_Tree1_9.java
@@ -0,0 +1,49 @@
+package problem_tree1_9;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class Problem_Tree1_9 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(input.readLine());
+        int[][] sequences = new int[4][n];
+
+        StringTokenizer tokenizer1 = new StringTokenizer(input.readLine());
+        StringTokenizer tokenizer2 = new StringTokenizer(input.readLine());
+        for (int i = 0; i < n; i++) {
+            sequences[0][i] = Integer.parseInt(tokenizer1.nextToken());
+            sequences[1][i] = Integer.parseInt(tokenizer2.nextToken());
+        }
+
+        Map<Integer, Integer> cntOfSum = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                int sum = sequences[0][i] + sequences[1][j];
+                cntOfSum.put(sum, cntOfSum.getOrDefault(sum, 0) + 1);
+            }
+        }
+
+        tokenizer1 = new StringTokenizer(input.readLine());
+        tokenizer2 = new StringTokenizer(input.readLine());
+        for (int i = 0; i < n; i++) {
+            sequences[2][i] = Integer.parseInt(tokenizer1.nextToken());
+            sequences[3][i] = Integer.parseInt(tokenizer2.nextToken());
+        }
+
+        int answer = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                answer += cntOfSum.getOrDefault(-sequences[2][i] - sequences[3][j], 0);
+            }
+        }
+        System.out.println(answer);
+    }
+
+}
